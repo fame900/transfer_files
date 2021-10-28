833ec1a9d672f08d99ff3fe0de6ae3d42b6c7943
EvgenijKorobov <korobov2393@gmail.com>
2021-10-28 10:03:21 +0300

    v1.3

--- file transfer.py
+++ file transfer.py
@@ -1,17 +1,8 @@
 import os
-import zipfile
-import docx
 import datetime as dt
 import shutil
 
 os.chdir(r'C:\python\file\123')  # переходим в директорию, где будем создавать архив
-shutil.make_archive('20211028', 'zip', root_dir=r'C:\python\file\20211028') # Создание архива
-
-day = dt.date.today()
-day_format = day.strftime('%d.%m.%Y')
-print(type(day))
-
-# Создание log
-doc = docx.Document()
-doc.add_paragraph('Тест  -  '+ day_format)
-doc.save(r'C:\python\file\123\log.docx')
+shutil.make_archive('archive', 'zip', root_dir=r'C:\python\file\archive') # создание архива
+day = dt.date.today().strftime('%d.%m.%Y') # время в нужном формате
+file = open('log.txt', 'w').write(day + ': logs') # создание log
