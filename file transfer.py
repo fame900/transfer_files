aaf5c003009c865e6f4b870ad861d559b0c880b9
EvgenijKorobov <korobov2393@gmail.com>
2021-10-28 15:53:58 +0300

    v1.4

--- file transfer.py
+++ file transfer.py
@@ -14,11 +14,11 @@ dir_list = [os.path.join(path_asu_nas, file) for file in os.listdir(path_asu_nas
 if dir_list:
     date_list = [[x, os.path.getctime(x)] for x in dir_list]  # Создадим список из путей к файлам и дат их создания.
     # Отсортируем список по дате создания в обратном порядке
-    sort_date_list = sorted(date_list, key=lambda x: x[1], reverse=True)[0][0]
+    sort_date_list = sorted(date_list, key=lambda x: x[1], reverse=True)[1][0]
     file_name = sort_date_list[-8:]  # создание имени для zip-файла
     os.chdir(path_archive_docx)  # переходим в директорию, где будем создавать архив
     archive = shutil.make_archive(file_name, 'zip', root_dir=sort_date_list)  # создание архива
     if archive[-12:] == file_name + '.zip':
-        print('Ready')
-        logging.info(f'Архив № {file_name} создан успешно!')
-    else: logging.info(f'Архив № {file_name} не создан')
+        print('Finish')
+        logging.info(f'Архив {file_name} создан успешно!')
+    else: logging.info(f'Архив {file_name} не создан')
