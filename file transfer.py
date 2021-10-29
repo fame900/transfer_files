18a11d19b26cac2bbdc5695580a1dab37b67b570
EvgenijKorobov <korobov2393@gmail.com>
2021-10-29 15:17:12 +0300

    v1.4

--- file transfer.py
+++ file transfer.py
@@ -1,24 +1,32 @@
 import os
 import shutil
 import logging
+from collections import Counter
 
 # логирование
 logging.basicConfig(format='%(asctime)15s - %(levelname)s:  %(message)s', datefmt='%d-%m-%Y %H:%M:%S',
                     level=logging.DEBUG,filename=r'\\asu-videoservr2\Arhiv_docs\log.txt')
 
 path_asu_nas = r'\\asu-nas\docserver'
-path_archive_docx = r'\\asu-videoservr2\Arhiv_docs'
-print('Please wait')
+path_archive_docx = r'\\asu-videoservr2\Arhiv_docs\docserver'
 
-dir_list = [os.path.join(path_asu_nas, file) for file in os.listdir(path_asu_nas)] # получаем пути к файлам
-if dir_list:
-    date_list = [[x, os.path.getctime(x)] for x in dir_list]  # Создадим список из путей к файлам и дат их создания.
-    # Отсортируем список по дате создания в обратном порядке
-    sort_date_list = sorted(date_list, key=lambda x: x[1], reverse=True)[1][0]
-    file_name = sort_date_list[-8:]  # создание имени для zip-файла
-    os.chdir(path_archive_docx)  # переходим в директорию, где будем создавать архив
-    archive = shutil.make_archive(file_name, 'zip', root_dir=sort_date_list)  # создание архива
-    if archive[-12:] == file_name + '.zip':
-        print('Finish')
-        logging.info(f'Архив {file_name} создан успешно!')
-    else: logging.info(f'Архив {file_name} не создан')
+dir_list_asu_nas = [os.path.join(path_asu_nas, file) for file in os.listdir(path_asu_nas)] # получаем пути к файлам
+dir_list_backup = [os.path.join(path_archive_docx, file) for file in os.listdir(path_archive_docx)]
+print(dir_list_asu_nas)
+print(dir_list_backup)
+
+folders_asu = [i[-8:] for i in dir_list_asu_nas]
+archives = [i[-12:-4] for i in dir_list_backup]
+print(folders_asu)
+print(archives)
+folders_noarchive=list(set(folders_asu)^set(archives))
+print(len(folders_noarchive))
+
+try:
+    if folders_noarchive!=[]:
+        for folder_name in folders_noarchive:
+            os.chdir(path_archive_docx)
+            shutil.make_archive(folder_name, 'zip', root_dir=f'''{path_asu_nas}\{folder_name}''')
+            logging.info(f'Архив {folder_name} создан успешно!')
+
+except: logging.exception('Error')
